357868e0f6f52d2dbd3668b6f355655b3e96a5e3
caoshuiluan <474389872@qq.com>
2019-09-19 23:25:49 +0800

    19号

--- src/components/fuzujian.vue
+++ src/components/fuzujian.vue
@@ -1,29 +1,90 @@
 <template>
-    <div>
-        <input type="text" v-model="name" @change="dataChange">
-        <p>{{name}}</p>
-        <zizujian :inputName="name"></zizujian>
+  <div>
+
+    <div :class="dakai?'dakai':''" class="getCode" @click="dakai1" style="margin-top:20px">
+      <div v-show="dakai">
+        <div class="changfangxing">
+          <div>
+            <img src="../assets/jianhao.png" alt style="color:white;margin-left:-40px" />
+          </div>
+        </div>
+        <div class="sanjiaoxing"></div>
+        <div class="box1"></div>
+      </div>
+      <div class="box2" v-show="!dakai">
+        <div class="changfangxing">
+          <div>
+            <img src="../assets/jiahao.png" alt style="color:white;margin-left:-40px" />
+          </div>
+        </div>
+        <div class="sanjiaoxing"></div>
+      </div>
     </div>
 
+    <div :class="open?'open':''" class="getCode" @click="dakai2" style="margin-top:20px">
+      <div v-show="open">
+        <div class="changfangxing">
+          <div>
+            <img src="../assets/jianhao.png" alt style="color:white;margin-left:-40px" />
+          </div>
+        </div>
+        <div class="sanjiaoxing"></div>
+        <div class="box3"></div>
+      </div>
+      <div class="box2" v-show="!open">
+        <div class="changfangxing">
+          <div>
+            <img src="../assets/jiahao.png" alt style="color:white;margin-left:-40px" />
+          </div>
+        </div>
+        <div class="sanjiaoxing"></div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import zizujian from '@/../src/components/zizujian'
 export default {
-   
-    data(){
-        return{
-            name:"wanglihong"
-        };
-        
+  data() {
+    return {
+      dakai: true,
+      open:true
+    };
+  },
+  methods: {
+    dakai1() {
+      this.dakai = !this.dakai;
     },
-    methods:{
-        dataChange(data){
-            this.inputName=data
-            
-        }
-    },
-     components:{
-        zizujian
+    dakai2(){
+        this.open = !this.open;
     }
-}
+  }
+};
 </script>
+<style >
+.box1 {
+  width: 400px;
+  height: 100px;
+  background: green;
+}
+.box3 {
+     width: 400px;
+  height: 300px;
+  background:hotpink;
+}
+.changfangxing {
+  width: 100px;
+  height: 20px;
+  background: red;
+  border-radius:0px 20px 1px 0px;
+}
+/* .sanjiaoxing {
+  width: 0;
+  height: 0;
+  border-top: 16px solid transparent;
+  border-left: 16px solid red;
+  border-bottom: 16px solid transparent;
+  position: absolute;
+  top: 54px;
+  left: 107px;
+} */
+</style>
