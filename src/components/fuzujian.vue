794809ab9ed2c40bf361850aeb33d78c5b84c431
caoshuiluan <474389872@qq.com>
2019-09-24 23:19:16 +0800

    111

--- src/components/fuzujian.vue
+++ src/components/fuzujian.vue
@@ -1,90 +1,48 @@
-<template>
-  <div>
 
-    <div :class="dakai?'dakai':''" class="getCode" @click="dakai1" style="margin-top:20px">
-      <div v-show="dakai">
-        <div class="changfangxing">
-          <div>
-            <img src="../assets/jianhao.png" alt style="color:white;margin-left:-40px" />
-          </div>
-        </div>
-        <div class="sanjiaoxing"></div>
-        <div class="box1"></div>
-      </div>
-      <div class="box2" v-show="!dakai">
-        <div class="changfangxing">
-          <div>
-            <img src="../assets/jiahao.png" alt style="color:white;margin-left:-40px" />
-          </div>
-        </div>
-        <div class="sanjiaoxing"></div>
-      </div>
-    </div>
 
-    <div :class="open?'open':''" class="getCode" @click="dakai2" style="margin-top:20px">
-      <div v-show="open">
-        <div class="changfangxing">
-          <div>
-            <img src="../assets/jianhao.png" alt style="color:white;margin-left:-40px" />
-          </div>
-        </div>
-        <div class="sanjiaoxing"></div>
-        <div class="box3"></div>
-      </div>
-      <div class="box2" v-show="!open">
-        <div class="changfangxing">
-          <div>
-            <img src="../assets/jiahao.png" alt style="color:white;margin-left:-40px" />
-          </div>
-        </div>
-        <div class="sanjiaoxing"></div>
-      </div>
-    </div>
-  </div>
-</template>
-<script>
-export default {
-  data() {
+ data() {
     return {
-      dakai: true,
-      open:true
+      // 假设这是后台传来的数据来源
+      data: [],
+      // 所有页面的数据
+      totalPage: [],
+      // 每页显示数量
+      pageSize: 5,
+      // 共几页
+      pageNum: 1,
+      // 当前显示的数据
+      dataShow: "",
+      // 默认当前显示第一页
+      currentPage: 0
     };
   },
-  methods: {
-    dakai1() {
-      this.dakai = !this.dakai;
-    },
-    dakai2(){
-        this.open = !this.open;
+步骤1：计算页数
+
+    // 这里简单模拟一下后台传过来的数据
+    for (let i = 0; i < 601; i++) {
+      this.data.push({ name: "liu" ,look:"very handsome"});
     }
-  }
-};
-</script>
-<style >
-.box1 {
-  width: 400px;
-  height: 100px;
-  background: green;
-}
-.box3 {
-     width: 400px;
-  height: 300px;
-  background:hotpink;
-}
-.changfangxing {
-  width: 100px;
-  height: 20px;
-  background: red;
-  border-radius:0px 20px 1px 0px;
-}
-/* .sanjiaoxing {
-  width: 0;
-  height: 0;
-  border-top: 16px solid transparent;
-  border-left: 16px solid red;
-  border-bottom: 16px solid transparent;
-  position: absolute;
-  top: 54px;
-  left: 107px;
-} */
-</style>
+    // 根据后台数据的条数和每页显示数量算出一共几页,得0时设为1 ;
+    this.pageNum = Math.ceil(this.data.length / this.pageSize) || 1;
+步骤二：根据页数对数据分组，并存进每一页
+
+   for (let i = 0; i < this.pageNum; i++) {
+      // 每一页都是一个数组 形如 [['第一页的数据'],['第二页的数据'],['第三页数据']]
+      // 根据每页显示数量 将后台的数据分割到 每一页,假设pageSize为5， 则第一页是1-5(对slice操作来说是0-5)条，第二页是6-10(对slice操作来说是5-10)条...
+      this.totalPage[i] = this.data.slice(this.pageSize * i, this.pageSize * (i + 1))
+    }
+步骤三：设置默认显示页，上一页下一个，只需要切换当前页面的索引值就行了
+
+    // 当前显示的内容
+    this.dataShow = this.totalPage[this.currentPage];
+    // 上一页和下一页
+    // 下一页
+    nextPage() {
+      if (this.currentPage == this.pageNum - 1) return ;
+      this.dataShow = this.totalPage[++this.currentPage];
+    },
+    // 上一页
+    prePage() {
+      if (this.currentPage == 0) return ;
+      this.dataShow = this.totalPage[--this.currentPage];
+
